d69bfc6694c0781ae73da149f632d86c3f259730
Son Hoang <hoangminhson29102001@gmail.com>
2026-09-23 12:20:48 +0900

    표 업뎃

--- results/update_tex.py
+++ results/update_tex.py
@@ -12,8 +12,13 @@ BASE_COLUMN = 6
 OURS_COLUMN = 7
 DELTA_COLUMN = 8
 EXCLUDED_SEEDS = {
+    "BattleZone": {2710},
+    "Boxing": {2710},
     "ChopperCommand": {1710},
     "Frostbite": {710},
+    "Hero": {2010},
+    "Jamesbond": {2000},
+    
 }
 
 
